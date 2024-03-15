5738a9bf01ed7f79e0c9d4d8345339de4a835f33
George Huang <gzh@alumni.princeton.edu>
2024-03-14 18:53:35 -0700

    changed adjust.js script in optikube-scripts

--- optikube-scripts/adjust.js
+++ optikube-scripts/adjust.js
@@ -2,9 +2,7 @@ import http from 'k6/http';
 
 export default function () {
   // Use an environment variable to set the thresholds
-  const upper = __ENV.UPPER || 40; 
-  const lower = __ENV.LOWER || 20;
-  const thres = __ENV.THRES || true; 
-  
-  http.get(`http://35.186.165.23:80/adjust?lowerLimit=${lower}&upperLimit=${upper}&costExceedsThreshold=${thres}`);
+  const target = __ENV.TARGET || 50; 
+
+  http.get(`http://35.186.165.23:80/adjust?cpuTarget=${target}`);
 }
