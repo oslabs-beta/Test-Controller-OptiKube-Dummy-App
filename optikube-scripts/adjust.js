fbf042944beee78f36be6ea8ee5f4de62c8f22d6
James Shea <jshea915@gmail.com>
2024-03-14 18:51:13 -0600

    added php scripts for testing

--- optikube-scripts/adjust.js
+++ optikube-scripts/adjust.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 
 export default function () {
   // Use an environment variable to set the thresholds
-  const upper = __ENV.UPPER || 80; 
-  const lower = __ENV.LOWER || 50;
+  const upper = __ENV.UPPER || 40; 
+  const lower = __ENV.LOWER || 20;
   const thres = __ENV.THRES || true; 
   
   http.get(`http://35.186.165.23:80/adjust?lowerLimit=${lower}&upperLimit=${upper}&costExceedsThreshold=${thres}`);
---
+++ php-scripts/test.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export let options = {
+    stages: [
+        { duration: '2m', target: 100 },
+        { duration: '5m', target: 100 },
+        { duration: '2m', target: 0 },
+    ],
+}
+
+const url = 'http://35.245.86.119:80';
+
+export default function () {
+    http.get(url)
+    sleep(1);
+}
